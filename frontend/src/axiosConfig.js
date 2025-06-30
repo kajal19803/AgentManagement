26ae1b6a6d07ebd8a48c91b331537562b4251714
Kajal Verma <kajalverma6263@gmail.com>
2025-06-30 12:51:41 +0530

    change base Url

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://agentmanagement-zf7q.onrender.com';
 axios.defaults.withCredentials = true;
 
 export default axios;
